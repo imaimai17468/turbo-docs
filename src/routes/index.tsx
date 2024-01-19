44f5a503a0e87f5b0e7aec71d8d412da409b6bec
imaimai17468 <tosiki_801_life@outlook.jp>
2024-01-19 23:07:32 +0900

    fix: Update main content in index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,14 +3,9 @@ import type { DocumentHead } from "@builder.io/qwik-city";
 
 export default component$(() => {
   return (
-    <>
-      <h1>Hi 👋</h1>
-      <p>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </p>
-    </>
+    <div>
+      <h1>Main Contents</h1>
+    </div>
   );
 });
 
