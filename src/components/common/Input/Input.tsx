09f88a14da11cc8379c12aa37ee7c3fb7f1432e4
imaimai17468 <tosiki_801_life@outlook.jp>
2024-01-20 21:22:16 +0900

    feat: Refactor Input component to include search icon

--- src/components/common/Input/Input.tsx
+++ src/components/common/Input/Input.tsx
@@ -1,21 +1,32 @@
 import { component$ } from "@builder.io/qwik";
 import { css } from "~/styled-system/css";
+import { LuSearch } from "@qwikest/icons/lucide";
 
 export const Input = component$(() => {
   return (
-    <input
-      class={css({
-        padding: "0.5rem",
-        fontSize: "1rem",
-        border: "1px solid #5a5a5a",
-        borderRadius: "0.25rem",
-        backgroundColor: "primary",
-        color: "text",
-        _focus: {
-          outline: "none",
-          borderColor: "accent",
-        }
-      })}
-    />
+    <div class={css({ position: "relative" })}>
+      <input
+        class={css({
+          padding: "0.5rem 0.5rem 0.5rem 2rem",
+          fontSize: "1rem",
+          border: "1px solid #5a5a5a",
+          borderRadius: "0.25rem",
+          backgroundColor: "primary",
+          color: "text",
+          _focus: {
+            outline: "none",
+            borderColor: "accent",
+          },
+        })}
+      />
+      <LuSearch
+        class={css({
+          position: "absolute",
+          top: "50%",
+          left: "0.5rem",
+          transform: "translateY(-50%)",
+        })}
+      />
+    </div>
   );
 });
