5b87c1af1bea8c8931e6d98ccd9f839977342ddc
imaimai17468 <tosiki_801_life@outlook.jp>
2024-01-23 22:39:51 +0900

    feat: Add Button variants

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -1,20 +1,49 @@
 import { component$, Slot } from "@builder.io/qwik";
-import { css } from "~/styled-system/css";
+import { cva } from "~/styled-system/css";
 
-export const Button = component$(() => {
-  return (
-    <button
-      class={css({
-        color: "text",
-        borderWidth: "1px",
+const buttonStyle = cva({
+  base: {
+    color: "text",
+    padding: "0.5rem 1rem",
+    transition: "all 0.2s ease",
+    cursor: "pointer",
+    _hover: { backgroundColor: "accent", color: "primary" },
+  },
+  variants: {
+    corner: {
+      square: { borderRadius: "0" },
+      rounded: { borderRadius: "0.25rem" },
+    },
+    outlined: {
+      true: { borderWidth: "1px" },
+      false: { borderWidth: "0" },
+    },
+    variant: {
+      primary: {
+        borderColor: "primary",
+        _hover: { backgroundColor: "primary", color: "text" },
+      },
+      background: {
+        borderColor: "background",
+        _hover: { backgroundColor: "background", color: "text" },
+      },
+      accent: {
         borderColor: "accent",
-        borderRadius: "0.25rem",
-        padding: "0.5rem 1rem",
-        transition: "all 0.2s ease",
-        cursor: "pointer",
         _hover: { backgroundColor: "accent", color: "primary" },
-      })}
-    >
+      },
+    },
+  },
+});
+
+type Props = {
+  corner: "square" | "rounded";
+  outlined: boolean;
+  variant: "primary" | "background" | "accent";
+};
+
+export const Button = component$<Props>(({ corner, outlined, variant }) => {
+  return (
+    <button class={buttonStyle({ corner, outlined, variant })}>
       <Slot />
     </button>
   );
