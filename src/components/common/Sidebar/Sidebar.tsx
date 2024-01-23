166e52df90d057d6d65721da1606f874c070170a
imaimai17468 <tosiki_801_life@outlook.jp>
2024-01-23 22:39:59 +0900

    feat: Update Sidebar component with new icons and styles

--- src/components/common/Sidebar/Sidebar.tsx
+++ src/components/common/Sidebar/Sidebar.tsx
@@ -3,7 +3,14 @@ import Image from "~/media/icon.png?jsx";
 import { css } from "~/styled-system/css";
 import { Input } from "../Input";
 import { Button } from "../Button";
-import { LuPlus } from "@qwikest/icons/lucide";
+import {
+  LuTag,
+  LuBook,
+  LuSettings,
+  LuGithub,
+  LuTrash,
+  LuPlusCircle,
+} from "@qwikest/icons/lucide";
 
 export const Sidebar = component$(() => {
   return (
@@ -19,22 +26,112 @@ export const Sidebar = component$(() => {
           backgroundColor: "primary",
           display: "flex",
           flexDirection: "column",
-          gap: "1rem",
+          justifyContent: "space-between",
         })}
       >
-        <div class={css({ display: "flex", alignItems: "center", gap: "0.5rem" })}>
-          <Image style={{ width: "48px" }} />
-          <h1 class={css({ fontSize: "1.5rem", fontWeight: "bold" })}>
-            Turbo Docs
-          </h1>
-        </div>
-        <Button>
-          <div class={css({ display: "flex", alignItems: "center", justifyContent: "center", gap: "0.5rem"})}>
-            <LuPlus />
-            <p>add Memo</p>
+        <div
+          class={css({
+            display: "flex",
+            flexDirection: "column",
+            gap: "1rem",
+          })}
+        >
+          <div
+            class={css({
+              display: "flex",
+              alignItems: "center",
+              gap: "0.5rem",
+            })}
+          >
+            <Image style={{ width: "48px" }} />
+            <h1 class={css({ fontSize: "1.5rem", fontWeight: "bold" })}>
+              Turbo Docs
+            </h1>
           </div>
-        </Button>
-        <Input placeholder="search" />
+          <Button corner="rounded" outlined variant="accent">
+            <div
+              class={css({
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                gap: "0.5rem",
+              })}
+            >
+              <LuPlusCircle />
+              <p>Create</p>
+            </div>
+          </Button>
+          <Input placeholder="search" />
+          <div class={css({ display: "flex", flexDirection: "column" })}>
+            <Button corner="square" outlined={false} variant="background">
+              <div
+                class={css({
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "start",
+                  gap: "0.5rem",
+                })}
+              >
+                <LuTag />
+                <p>Tags</p>
+              </div>
+            </Button>
+            <Button corner="rounded" outlined={false} variant="background">
+              <div
+                class={css({
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "start",
+                  gap: "0.5rem",
+                })}
+              >
+                <LuBook />
+                <p>Bookmark</p>
+              </div>
+            </Button>
+            <Button corner="rounded" outlined={false} variant="background">
+              <div
+                class={css({
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "start",
+                  gap: "0.5rem",
+                })}
+              >
+                <LuTrash />
+                <p>Trash</p>
+              </div>
+            </Button>
+          </div>
+          <hr class={css({ borderColor: "border" })} />
+          <Button corner="rounded" outlined={false} variant="background">
+            <div
+              class={css({
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "start",
+                gap: "0.5rem",
+              })}
+            >
+              <LuSettings />
+              <p>Settings</p>
+            </div>
+          </Button>
+        </div>
+        <div
+          class={css({
+            display: "flex",
+            alignItems: "center",
+            gap: "0.5rem",
+          })}
+        >
+          <p class={css({ fontSize: "0.75rem", color: "text" })}>
+            Turbo Docs v0.1.0
+          </p>
+          <Button corner="rounded" outlined={false} variant="background">
+            <LuGithub />
+          </Button>
+        </div>
       </div>
     </div>
   );
