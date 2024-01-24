f75ff1ac6008b45ea5f2f08c18bba36383e54a29
imaimai17468 <tosiki_801_life@outlook.jp>
2024-01-24 23:23:48 +0900

    feat: Add VStack component

---
+++ src/components/common/VStack/VStack.tsx
@@ -0,0 +1,44 @@
+import { component$, Slot } from "@builder.io/qwik";
+import { cva } from "~/styled-system/css";
+
+type Props = {
+  gap: "xs" | "sm" | "md" | "lg";
+  justifyContent: "start" | "center" | "end" | "between" | "around";
+  alignItems: "start" | "center" | "end" | "stretch";
+};
+
+const VStackStyle = cva({
+  base: {
+    display: "flex",
+    flexDirection: "column",
+  },
+  variants: {
+    gap: {
+      xs: { gap: "0.25rem" },
+      sm: { gap: "0.5rem" },
+      md: { gap: "1rem" },
+      lg: { gap: "2rem" },
+    },
+    justifyContent: {
+      start: { justifyContent: "start" },
+      center: { justifyContent: "center" },
+      end: { justifyContent: "end" },
+      between: { justifyContent: "space-between" },
+      around: { justifyContent: "space-around" },
+    },
+    alignItems: {
+      start: { alignItems: "start" },
+      center: { alignItems: "center" },
+      end: { alignItems: "end" },
+      stretch: { alignItems: "stretch" },
+    },
+  },
+});
+
+export const VStack = component$<Props>(({ gap, justifyContent, alignItems }) => {
+  return (
+    <div class={VStackStyle({ gap, justifyContent, alignItems })}>
+      <Slot />
+    </div>
+  );
+});
