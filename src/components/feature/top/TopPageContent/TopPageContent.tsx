df2a8860083e1cdaf3b9bdcd0172cedf4094c1b3
imaimai17468 <tosiki_801_life@outlook.jp>
2024-01-27 21:16:15 +0900

    style: Refactor TopPageContent component to use CSS grid for layout

--- src/components/feature/top/TopPageContent/TopPageContent.tsx
+++ src/components/feature/top/TopPageContent/TopPageContent.tsx
@@ -1,11 +1,19 @@
 import { component$ } from "@builder.io/qwik";
 import { PostCard } from "~/components/common/PostCard";
+import { css } from "~/styled-system/css";
 
 export const TopPageContent = component$(() => {
   return (
-    <div>
+    <div
+      class={css({
+        width: "100%",
+        display: "grid",
+        gridTemplateColumns: "repeat(auto-fill, minmax(16rem,1fr))",
+        gap: "2rem",
+      })}
+    >
       {Array.from({ length: 10 }).map((_, i) => (
-        <PostCard key={i} title={`test post ${i}`} />
+        <PostCard key={i} title={`test post ${i}`} lastModifiedAt={new Date()} />
       ))}
     </div>
   );
