3f885f4a46438ad8eccb5bec311fe494bd0b5996
imaimai17468 <tosiki_801_life@outlook.jp>
2024-01-28 17:32:55 +0900

    feat: Refactor Logo component to include a Link to the index page

--- src/components/common/Logo/Logo.tsx
+++ src/components/common/Logo/Logo.tsx
@@ -2,14 +2,18 @@ import { component$ } from "@builder.io/qwik";
 import { HStack } from "~/styled-system/jsx";
 import Image from "~/media/icon.png?jsx";
 import { css } from "~/styled-system/css";
+import { Link } from "@builder.io/qwik-city";
+import { CLIENT_PATH } from "~/components/constants/clientPath";
 
 export const Logo = component$(() => {
   return (
-    <HStack gap="sm" alignItems="center" justifyContent="center">
-      <Image style={{ width: "48px" }} />
-      <h1 class={css({ fontSize: "1.5rem", fontWeight: "bold" })}>
-        Turbo Docs
-      </h1>
-    </HStack>
+    <Link href={CLIENT_PATH.index}>
+      <HStack gap="sm" alignItems="center" justifyContent="center">
+        <Image style={{ width: "48px" }} />
+        <h1 class={css({ fontSize: "1.5rem", fontWeight: "bold" })}>
+          Turbo Docs
+        </h1>
+      </HStack>
+    </Link>
   );
 });
