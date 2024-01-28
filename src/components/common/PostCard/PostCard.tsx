9d776c23bb2fff109063a927ac34083ca8a80ae8
imaimai17468 <tosiki_801_life@outlook.jp>
2024-01-28 17:29:15 +0900

    feat: Add link to post in PostCard component

--- src/components/common/PostCard/PostCard.tsx
+++ src/components/common/PostCard/PostCard.tsx
@@ -3,6 +3,8 @@ import { css } from "~/styled-system/css";
 import dayjs from "dayjs";
 import { LuBookmark, LuMenu, LuTimer } from "@qwikest/icons/lucide";
 import { HStack, VStack } from "~/styled-system/jsx";
+import { CLIENT_PATH } from "~/components/constants/clientPath";
+import { Link } from "@builder.io/qwik-city";
 
 type Props = {
   title: string;
@@ -10,31 +12,33 @@ type Props = {
 };
 
 export const PostCard = component$<Props>(({ title, lastModifiedAt }) => (
-  <div
-    class={css({
-      color: "text_black",
-      width: "100%",
-      height: "8rem",
-      backgroundColor: "background_white",
-      borderRadius: "0.5rem",
-      cursor: "pointer",
-      padding: "1rem",
-      display: "flex",
-      flexDirection: "column",
-    })}
-  >
-    <VStack gap="0.25rem" alignItems="start" flexGrow="1">
-      <h2>{title}</h2>
-      <HStack gap="0.25rem" class={css({ fontSize: "0.75rem" })}>
-        <LuTimer />
-        <p class={css({ color: "text_gray" })}>
-          {dayjs(lastModifiedAt).format("YYYY MM DD")}
-        </p>
+  <Link href={CLIENT_PATH.post.replace("[id]", "test")}>
+    <div
+      class={css({
+        color: "text_black",
+        width: "100%",
+        height: "8rem",
+        backgroundColor: "background_white",
+        borderRadius: "0.5rem",
+        cursor: "pointer",
+        padding: "1rem",
+        display: "flex",
+        flexDirection: "column",
+      })}
+    >
+      <VStack gap="0.25rem" alignItems="start" flexGrow="1">
+        <h2>{title}</h2>
+        <HStack gap="0.25rem" class={css({ fontSize: "0.75rem" })}>
+          <LuTimer />
+          <p class={css({ color: "text_gray" })}>
+            {dayjs(lastModifiedAt).format("YYYY MM DD")}
+          </p>
+        </HStack>
+      </VStack>
+      <HStack justifyContent="end">
+        <LuBookmark />
+        <LuMenu />
       </HStack>
-    </VStack>
-    <HStack justifyContent="end">
-      <LuBookmark />
-      <LuMenu />
-    </HStack>
-  </div>
+    </div>
+  </Link>
 ));
