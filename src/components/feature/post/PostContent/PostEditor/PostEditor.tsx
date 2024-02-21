8663e830f8e3f469642e2c84e842d5658f95450a
imaimai17468 <tosiki_801_life@outlook.jp>
2024-02-21 22:32:14 +0900

    feat: 入力された値の状態を取得するように

--- src/components/feature/post/PostContent/PostEditor/PostEditor.tsx
+++ src/components/feature/post/PostContent/PostEditor/PostEditor.tsx
@@ -1,9 +1,29 @@
-/** @jsxImportSource react */
+import { component$, useSignal } from "@builder.io/qwik";
+import { css } from "~/styled-system/css";
+import type { PostTexts } from "../../../../../types/PostText";
 
-import { qwikify$ } from "@builder.io/qwik-react";
+export const PostEditor = component$(() => {
+  const texts = useSignal<PostTexts>([{ text: "" }]);
 
-const Editor = () => {
-  return <div>Editor</div>;
-};
-
-export const PostEditor = qwikify$(Editor);
+  return (
+    <>
+      {texts.value.map((text, index) => (
+        <input
+          key={index}
+          class={css({
+            background: "transparent",
+            color: "text",
+            fontSize: "1rem",
+            _focus: {
+              outline: "none",
+            },
+          })}
+          value={text.text}
+          onInput$={(_, el) => {
+            texts.value[index].text = el.value;
+          }}
+        />
+      ))}
+    </>
+  );
+});
---
+++ src/types/PostText.ts
@@ -0,0 +1,5 @@
+export type PostText = {
+  text: string;
+};
+
+export type PostTexts = PostText[];
