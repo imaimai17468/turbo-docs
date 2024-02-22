0f67337be202e04e2f8966fa14362396a6a56c0b
imaimai17468 <tosiki_801_life@outlook.jp>
2024-02-22 22:04:31 +0900

    feat: エディタの実装

--- src/components/feature/post/PostContent/PostEditor/PostEditor.tsx
+++ src/components/feature/post/PostContent/PostEditor/PostEditor.tsx
@@ -1,29 +1,51 @@
-import { component$, useSignal } from "@builder.io/qwik";
+import { component$, useComputed$, useSignal } from "@builder.io/qwik";
 import { css } from "~/styled-system/css";
-import type { PostTexts } from "../../../../../types/PostText";
+import { HStack } from "~/styled-system/jsx";
 
 export const PostEditor = component$(() => {
-  const texts = useSignal<PostTexts>([{ text: "" }]);
+  const elementString = useSignal<string>();
+  const html = useComputed$(() => {
+    if (!elementString.value) return;
+    const divider = elementString.value
+      .split("<div>")
+      .map((item) => "<div>" + item.replace(/<br>|<\/div>/g, "") + "</div>");
+    const mdParser = divider
+      .map((item) => {
+        return item
+          .replace(/### (.+)/g, "<h3 style='font-size:1.5rem'>$1</h3>")
+          .replace(/## (.+)/g, "<h2 style='font-size:2rem'>$1</h2>")
+          .replace(/# (.+)/g, "<h1 style='font-size:3rem'>$1</h1>")
+          .replace(/\*\*(.+)\*\*/g, "<b>$1</b>")
+          .replace(/~~(.+)~~/g, "<s>$1</s>");
+      })
+      .join("");
+
+    console.log(mdParser);
+    return mdParser;
+  });
 
   return (
-    <>
-      {texts.value.map((text, index) => (
-        <input
-          key={index}
-          class={css({
-            background: "transparent",
-            color: "text",
-            fontSize: "1rem",
-            _focus: {
-              outline: "none",
-            },
-          })}
-          value={text.text}
-          onInput$={(_, el) => {
-            texts.value[index].text = el.value;
-          }}
-        />
-      ))}
-    </>
+    <HStack width="100%" height="83vh" alignItems="start">
+      <div
+        contentEditable="true"
+        class={css({
+          width: "50%",
+          height: "100%",
+          _focus: {
+            outline: "none",
+          },
+          overflow: "scroll",
+        })}
+        onInput$={(_, el) => (elementString.value = el.innerHTML)}
+      />
+      <div
+        class={css({
+          height: "83vh",
+          borderLeft: "1px solid",
+          borderColor: "border",
+        })}
+      />
+      <div class={css({ width: "50%" })} dangerouslySetInnerHTML={html.value} />
+    </HStack>
   );
 });
