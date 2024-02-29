32831b44f49a00516306af21891366fd28ae5647
imaimai17468 <tosiki_801_life@outlook.jp>
2024-03-01 01:55:42 +0900

    fix: linkの配置を修正

--- src/components/common/PostCard/PostCard.tsx
+++ src/components/common/PostCard/PostCard.tsx
@@ -1,10 +1,11 @@
 import { component$ } from "@builder.io/qwik";
 import { css } from "~/styled-system/css";
 import dayjs from "dayjs";
-import { LuBookmark, LuMenu, LuTimer } from "@qwikest/icons/lucide";
+import { LuMenu, LuTimer } from "@qwikest/icons/lucide";
 import { HStack, VStack } from "~/styled-system/jsx";
 import { CLIENT_PATH } from "~/constants/clientPath";
 import { Link } from "@builder.io/qwik-city";
+import { BookmarkButton } from "./BookmarkButton";
 
 type Props = {
   title: string;
@@ -12,33 +13,51 @@ type Props = {
 };
 
 export const PostCard = component$<Props>(({ title, lastModifiedAt }) => (
-  <Link href={CLIENT_PATH.post.replace("[id]", "test")}>
-    <div
+  <div
+    class={css({
+      color: "text_black",
+      width: "100%",
+      height: "8rem",
+      backgroundColor: "background_white",
+      borderRadius: "0.5rem",
+      padding: "1rem",
+      display: "flex",
+      flexDirection: "column",
+      position: "relative",
+    })}
+  >
+    <Link
+      href={CLIENT_PATH.post.replace("[id]", "test")}
       class={css({
-        color: "text_black",
         width: "100%",
-        height: "8rem",
-        backgroundColor: "background_white",
-        borderRadius: "0.5rem",
-        cursor: "pointer",
-        padding: "1rem",
-        display: "flex",
-        flexDirection: "column",
+        height: "100%",
+        position: "absolute",
+        bottom: "0",
+        right: "0",
+      })}
+    />
+    <VStack gap="0.25rem" alignItems="start" flexGrow="1">
+      <h2>{title}</h2>
+      <HStack gap="0.25rem" class={css({ fontSize: "0.75rem" })}>
+        <LuTimer />
+        <p class={css({ color: "text_gray" })}>
+          {dayjs(lastModifiedAt).format("YYYY MM DD")}
+        </p>
+      </HStack>
+    </VStack>
+    <HStack
+      justifyContent="end"
+      class={css({
+        zIndex: 1,
+        position: "absolute",
+        bottom: "0",
+        right: "0",
+        paddingBottom: "0.5rem",
+        paddingRight: "1rem",
       })}
     >
-      <VStack gap="0.25rem" alignItems="start" flexGrow="1">
-        <h2>{title}</h2>
-        <HStack gap="0.25rem" class={css({ fontSize: "0.75rem" })}>
-          <LuTimer />
-          <p class={css({ color: "text_gray" })}>
-            {dayjs(lastModifiedAt).format("YYYY MM DD")}
-          </p>
-        </HStack>
-      </VStack>
-      <HStack justifyContent="end">
-        <LuBookmark />
-        <LuMenu />
-      </HStack>
-    </div>
-  </Link>
+      <BookmarkButton />
+      <LuMenu />
+    </HStack>
+  </div>
 ));
