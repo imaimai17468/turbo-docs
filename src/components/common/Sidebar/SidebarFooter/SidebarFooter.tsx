3dbed2a3de5bf2ebd688273bb33b025d0e3aaf62
imaimai17468 <tosiki_801_life@outlook.jp>
2024-03-01 22:54:45 +0900

    fix: Sidebarのフッターのスタイルを修正

--- src/components/common/Sidebar/SidebarFooter/SidebarFooter.tsx
+++ src/components/common/Sidebar/SidebarFooter/SidebarFooter.tsx
@@ -2,10 +2,12 @@ import { component$ } from "@builder.io/qwik";
 import { css } from "~/styled-system/css";
 import { LuGithub } from "@qwikest/icons/lucide";
 import { HStack } from "~/styled-system/jsx";
+import { Link } from "@builder.io/qwik-city";
+import { Button } from "~/components/ui/Button";
 
 export const SidebarFooter = component$(() => {
   return (
-    <HStack gap="0.5rem" alignItems="center" justifyContent="start">
+    <HStack gap="1rem" alignItems="center" justifyContent="start">
       <p
         class={css({
           fontSize: "0.75rem",
@@ -15,16 +17,16 @@ export const SidebarFooter = component$(() => {
       >
         Turbo Docs v0.0.1
       </p>
-      <div class={css({ width: "fit-content" })}>
-        <a
-          href="https://github.com/imaimai17468/turbo-docs-web"
-          title="GitHub Repository"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
+      <Link
+        href="https://github.com/imaimai17468/turbo-docs-web"
+        target="_blank"
+        rel="noopener noreferrer"
+        title="GitHub Repository"
+      >
+        <Button isIcon areaLabel="GitHub">
           <LuGithub />
-        </a>
-      </div>
+        </Button>
+      </Link>
     </HStack>
   );
 });
