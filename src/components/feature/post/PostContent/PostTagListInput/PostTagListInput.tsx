1b8358eef01e1d2e4db21aa8df3499426083c9ce
imaimai17468 <tosiki_801_life@outlook.jp>
2024-03-02 00:06:21 +0900

    feat: tagの表示内容の追加

--- src/components/feature/post/PostContent/PostTagListInput/PostTagListInput.tsx
+++ src/components/feature/post/PostContent/PostTagListInput/PostTagListInput.tsx
@@ -1,23 +1,58 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useSignal, useComputed$ } from "@builder.io/qwik";
 import { LuTag } from "@qwikest/icons/lucide";
 import { css } from "~/styled-system/css";
-import { HStack } from "~/styled-system/jsx";
+import { HStack, VStack } from "~/styled-system/jsx";
 
 export const PostTagListInput = component$(() => {
+  const tagText = useSignal<string>();
+  const tagList = useComputed$(() => {
+    if (!tagText.value) return [];
+    const splitTagText = tagText.value.split(" ");
+
+    if (splitTagText.length < 1) return [];
+
+    const tagList = splitTagText.slice(0, -1);
+
+    return tagList;
+  });
+
   return (
-    <HStack gap="1rem">
-      <LuTag class={css({ height: "1rem" })} />
-      <input
-        class={css({
-          background: "transparent",
-          color: "text",
-          fontSize: "1rem",
-          _focus: {
-            outline: "none",
-          },
-        })}
-        placeholder="Tags"
-      />
-    </HStack>
+    <VStack alignItems="start">
+      <HStack gap="1rem">
+        <LuTag class={css({ height: "1rem" })} />
+        <input
+          class={css({
+            background: "transparent",
+            color: "text",
+            fontSize: "1rem",
+            _focus: {
+              outline: "none",
+            },
+          })}
+          placeholder="Tags"
+          bind:value={tagText}
+        />
+      </HStack>
+      {tagList.value.length !== 0 && (
+        <HStack>
+          {tagList.value.map((tag, index) => (
+            <div
+              key={index}
+              class={css({
+                background: "accent",
+                color: "text_black",
+                rounded: "0.5rem",
+                padding: "0 0.5rem",
+                minWidth: "3rem",
+                display: "flex",
+                justifyContent: "center",
+              })}
+            >
+              {tag}
+            </div>
+          ))}
+        </HStack>
+      )}
+    </VStack>
   );
 });
